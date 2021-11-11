a86e6528b2a467c5588141c462082715f67a800f
Pavan Kumar <34813177+pavan-kumar-99@users.noreply.github.com>
2021-11-11 23:06:48 +0530

    Update ing-ref.sh

--- GKE/Ingress/ing-ref.sh
+++ GKE/Ingress/ing-ref.sh
@@ -1,4 +1,7 @@
 ## Thanos Cross cluster reference
 
+##Additional targets secret
+https://github.com/prometheus-operator/prometheus-operator/blob/master/Documentation/additional-scrape-config.md
+
 ## TODO Add sample files
 https://thanos.io/tip/operating/cross-cluster-tls-communication.md/
