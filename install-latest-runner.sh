bf40f638dfbe5278141d1ca462a48bbe9cb450c0
Pavan Kumar <34813177+pavan-kumar-99@users.noreply.github.com>
2021-11-30 04:04:35 +0530

    Update install-latest-runner.sh

--- install-latest-runner.sh
+++ install-latest-runner.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-
-export latest_version_label=$(curl -s -X GET 'https://api.github.com/repos/actions/runner/releases/latest' | jq -r '.tag_name') && latest_version=$(echo ${latest_version_label:1}) && export runner_file="actions-runner-${runner_plat}-x64-${latest_version}.tar.gz" && export runner_url="https://github.com/actions/runner/releases/download/${latest_version_label}/${runner_file}" && curl -O -L ${runner_url} && tar xzf "./${runner_file}"
+set -x
+runner_plat=linux
+latest_version_label=$(curl -s -X GET 'https://api.github.com/repos/actions/runner/releases/latest' | jq -r '.tag_name') 
+latest_version=$(echo ${latest_version_label:1}) 
+runner_file="actions-runner-${runner_plat}-x64-${latest_version}.tar.gz" 
+runner_url="https://github.com/actions/runner/releases/download/${latest_version_label}/${runner_file}" 
+curl -O -L ${runner_url} 
+tar xzf "./${runner_file}"
