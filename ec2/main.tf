b87e4f79b218170c0289dd669054673103a622a6
Pavan Kumar <34813177+pavan-kumar-99@users.noreply.github.com>
2022-03-27 10:39:24 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,3 +1,13 @@
+## Remote S3 Data Block
+terraform {
+  backend "s3" {
+    bucket = "atlantisterraform"
+    key    = "terraform/atlantisterraform/state"
+    region = "ap-south-1"
+  }
+}
+
+## Create an EC2 Instance
 resource "aws_instance" "web" {
   ami               = "ami-0851b76e8b1bce90b"
   instance_type     = "t3.micro"
