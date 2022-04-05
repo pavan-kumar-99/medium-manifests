f174bb1e1ccdb4d46474b70389878527d8794b0e
Pavan Kumar <34813177+pavan-kumar-99@users.noreply.github.com>
2022-04-05 20:50:12 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -10,7 +10,7 @@ terraform {
 ## Create an EC2 Instance
 resource "aws_instance" "web" {
   ami               = "ami-0851b76e8b1bce90b"
-  instance_type     = "t2.medium"
+  instance_type     = "t3.medium"
   availability_zone = "ap-south-1b"
   tags = {
     Name = "atlantis-medium"
