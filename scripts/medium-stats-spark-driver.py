9b0ab1c412af91dd1ba368f46ca0764729218f11
Pavan Kumar <34813177+pavan-kumar-99@users.noreply.github.com>
2024-05-08 22:06:21 -0400

    Update medium-stats-spark-driver.py

--- scripts/medium-stats-spark-driver.py
+++ scripts/medium-stats-spark-driver.py
@@ -1,9 +1,10 @@
+import argparse
 import boto3
+import os
 import botocore
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
-from pyspark.sql.window import Window
 from pyspark.sql.types import *
 
 # Define a function to download files from S3
@@ -26,47 +27,75 @@ def download_from_s3(bucket_name, key, local_path):
         else:
             raise
 
-# Download files from S3
-download_from_s3("your-bucket-name", "path/to/file.json", "local/file.json")
+def process_data(local_path):
+    """
+    Process data from JSON files and return the joined DataFrame.
 
-# Set up Spark session and configuration
-conf = SparkConf()
-spark = SparkSession.builder \
-    .appName("Medium Stats Pavan") \
-    .config(conf=conf) \
-    .getOrCreate()
+    Args:
+    - bucket_name: Name of the S3 bucket.
+    - key: Key (path) of the file in the S3 bucket.
+    - local_path: Local path where the file will be downloaded.
 
-# Define schema for the data
-schema = StructType([
-    StructField("dayStartsAt", LongType(), True),
-    StructField("id", IntegerType(), True),
-    StructField("Date", LongType(), True),
-    StructField("readersThatClappedCount", LongType(), True),
-    StructField("readersThatReadCount", LongType(), True),
-    StructField("readersThatRepliedCount", LongType(), True),
-    StructField("readersThatViewedCount", LongType(), True),
-    StructField("article_id", LongType(), True)
-])
+    Returns:
+    - Joined DataFrame.
+    """
+    # Initialize Spark session
+    spark = SparkSession.builder \
+        .appName("Medium Stats Pavan") \
+        .getOrCreate()
 
-# Read JSON files into DataFrames
-df_all = spark.read.option("multiLine", "true").option("mode", "PERMISSIVE").schema(schema).json("local")
-df = spark.read.option("multiLine", "true").option("mode", "PERMISSIVE").schema(schema).json("local")
-df_posts = spark.read.option("multiLine", "true").option("mode", "PERMISSIVE").json("local/posts_info.json")
+    statsSchema = StructType([
+        StructField("dayStartsAt", LongType(), True),
+        StructField("id", IntegerType(), True),
+        StructField("Date", LongType(), True),
+        StructField("readersThatClappedCount", LongType(), True),
+        StructField("readersThatReadCount", LongType(), True),
+        StructField("readersThatRepliedCount", LongType(), True),
+        StructField("readersThatViewedCount", LongType(), True),
+        StructField("article_id", LongType(), True)
+    ])
+    
+    articlesSchema = StructType([
+        StructField("article_id", LongType(), True),
+        StructField("title", StringType(), True),
+        StructField("url", StringType(), True)
+    ])
 
-# Data transformations
-df = df.dropDuplicates()  # Drop duplicate rows
-df = df.withColumn('article_id', lit('ef99f7a4e417'))  # Set article_id
-df = df.withColumn('Date', to_date(from_unixtime(floor(col('dayStartsAt') / 1000), 'yyyy-MM-dd HH:mm:ss')))  # Convert timestamp to date
-columns_to_drop = ['__typename', 'membershipType', 'dayStartsAt', 'readersThatInitiallyFollowedAuthorFromThisPostCount', 'readersThatHighlightedCount']
-df = df.drop(*columns_to_drop)  # Drop unnecessary columns
-window = Window.orderBy(col('Date'))
-df = df.withColumn('id', row_number().over(window))  # Add row number
+    # Read JSON files into DataFrames
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(script_dir, 'articles.json')
+    df = spark.read.option("multiLine", "true").option("mode", "PERMISSIVE").schema(articlesSchema).json(local_path)
+    df_posts = spark.read.option("multiLine", "true").option("mode", "PERMISSIVE").json(file_path)
 
-# Join operation
-df_join = df.join(df_posts, df['article_id'] == df_posts['article_id'], 'inner').drop(df.article_id)
+    # Data transformations
+    df = df.dropDuplicates()  # Drop duplicate rows
+    df_posts = df_posts.dropDuplicates()
+    df = df.withColumn('Date', to_date(from_unixtime(floor(col('dayStartsAt') / 1000), 'yyyy-MM-dd HH:mm:ss')))
+    columns_to_drop = ['__typename', 'membershipType', 'dayStartsAt', 'readersThatInitiallyFollowedAuthorFromThisPostCount', 'readersThatHighlightedCount']
+    df = df.drop(*columns_to_drop) 
 
-# Count rows in df_all DataFrame
-total_rows = df_all.count()
+    # Join operation
+    df_join = df.join(df_posts, df['article_id'] == df_posts['article_id'], 'inner').drop(df.article_id).withColumnRenamed("readersThatViewedCount", "Viewers")
 
-# Print total rows count
-print("Total rows in df_all DataFrame:", total_rows)
+    return df_join
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process files from S3 and perform data transformations.')
+    parser.add_argument('bucket_name', type=str, help='Name of the S3 bucket')
+    parser.add_argument('key', type=str, help='Key (path) of the file in the S3 bucket')
+    parser.add_argument('local_path', type=str, help='Local path where the file will be downloaded')
+    parser.add_argument('ingest_mode', type=str, choices=['append', 'create'], help='Ingestion mode: append or create')
+    args = parser.parse_args()
+
+    # Download files from S3
+    download_from_s3(args.bucket_name, args.key, args.local_path)
+
+    # Process data
+    df = process_data(args.local_path)
+
+    if args.ingest_mode == 'append':
+        df.write.mode('append').saveAsTable("my_table")
+        print("Data appended to table 'my_table'")
+    elif args.ingest_mode == 'create':
+        print("Data created table 'my_table'")
+        df.writeTo("iceberg_catalog.medium-stats.article-stats").append()
