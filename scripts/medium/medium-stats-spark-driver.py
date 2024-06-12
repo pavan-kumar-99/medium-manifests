7dba05b493fc7d3abb85d4365a649a9459d60f1c
Pavan Kumar <34813177+pavan-kumar-99@users.noreply.github.com>
2024-06-11 23:02:45 -0400

    Update medium-stats-spark-driver.py

--- scripts/medium/medium-stats-spark-driver.py
+++ scripts/medium/medium-stats-spark-driver.py
@@ -79,12 +79,8 @@ def process_data(bucket_name, bucket_prefix, local_path):
         )
         .config(f"spark.sql.defaultCatalog", f"{catalog_name}")
         .config(
-            f"spark.sql.catalog.{catalog_name}.s3.endpoint",
-            "http://minio-server.minio:9000",
-        )
-        .config(
             f"spark.sql.catalog.{catalog_name}.s3a.endpoint",
-            "http://minio-server.minio:9000",
+            "http://minio-server.minio:9000/",
         )
         .config(
             "spark.sql.extensions",
@@ -208,7 +204,7 @@ if __name__ == "__main__":
     local_path = "/usr/stats/" + args.key
     bucket_name = "medium-stats"
     iceberg_bucket_name = "iceberg-table"
-    iceberg_bucket_prefix = "iceberg-tables/"
+    iceberg_bucket_prefix = "warehouse/"
     temp_table_name = "mediumstatstemp"
 
     download_from_s3(bucket_name, args.key, local_path)
